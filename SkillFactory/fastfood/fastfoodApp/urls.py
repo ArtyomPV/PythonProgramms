b9d9005c683277cfc2ded501d8bcd3ed9be0ef05
Artyom <itcoding76@gmail.com>
2023-08-09 09:14:13 +0200

    added productDetailView

--- SkillFactory/fastfood/fastfoodApp/urls.py
+++ SkillFactory/fastfood/fastfoodApp/urls.py
@@ -1,7 +1,9 @@
-from  django.urls import  path
-from .views import ProductsList
+from django.urls import path
+from .views import ProductsList, ProductDetail
 
 urlpatterns = [
-    path('', ProductsList.as_view()) # класс ProductsList надо предоставить класс в
+    path('', ProductsList.as_view()), # класс ProductsList надо предоставить класс в
     # виде view, для этого вызываем метод as_view
+    path('<int:pk>', ProductDetail.as_view()),  # pk -- это первичный ключ товара,
+    # который будет выводиться у нас в шаблон
 ]
--- SkillFactory/fastfood/fastfoodApp/views.py
+++ SkillFactory/fastfood/fastfoodApp/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
+
 
 class ProductsList(ListView):
     model = Product
-    template_name = 'fastfoodApp/products.html' #имя шаблона, в котором будет
+    template_name = 'fastfoodApp/products.html'  # имя шаблона, в котором будет
     # лежать HTML, в нем будут инструкции, о том как будут выводиться наши объекты
-    context_object_name = 'products' #имя списка в котором лежат все объекты,
+    context_object_name = 'products'  # имя списка в котором лежат все объекты,
     # его нужно указать, чтобы обратиться к самому списку объктов через HTML шаблон
 
 
+# создаём представление, в котором будут детали конкретного отдельного товара
+class ProductDetail(DetailView):
+    model = Product  # модель всё та же, но мы хотим получать детали конкретно
+    # отдельного товара
+    template_name = 'fastfoodApp/product.html'  # название шаблона будет product.html
+    context_object_name = 'product'  # название объекта
