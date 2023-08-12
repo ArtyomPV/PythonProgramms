edc6e83db0567dff782d745239213f817ca98978
Artyom <itcoding76@gmail.com>
2023-08-12 10:26:01 +0200

    used new templates with HttpResponse

--- Dronov/samplesite/bboard/views.py
+++ Dronov/samplesite/bboard/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import *
 
 
 def index(request):
-    s = 'List of ads \r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
