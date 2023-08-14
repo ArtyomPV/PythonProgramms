4fc1a92b1a015fc126cd3abc54ae83b4d44bb160
Artyom <itcoding76@gmail.com>
2023-08-14 07:54:20 +0200

    added paginated news output and renamed urls from 'post' to 'news'

--- SkillFactory/projectNewsPaper/newspaper/urls.py
+++ SkillFactory/projectNewsPaper/newspaper/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from  .views import PostsList, PostDetail
+from  .views import PostsList, PostDetail, PostsView
 
+app_name = 'newspaper'
 urlpatterns = [
     path('', PostsList.as_view()),
-    path('<int:pk>', PostDetail.as_view())
+    path('<int:pk>', PostDetail.as_view()),
+    path('news/', PostsView.as_view()),
 ]
--- SkillFactory/projectNewsPaper/newspaper/views.py
+++ SkillFactory/projectNewsPaper/newspaper/views.py
@@ -1,5 +1,10 @@
+from django.views import View
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Post
+
+
 # Create your views here.
 
 
@@ -7,10 +12,22 @@ class PostsList(ListView):
     model = Post
     template_name = 'newspaper/posts.html'
     context_object_name = 'posts'
-    queryset = Post.objects.order_by('-data_post_creation')
+    ordering = ['-data_post_creation']
+    paginate_by = 3
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'newspaper/post.html'
     context_object_name = 'post'
+
+
+class PostsView(View):
+    def get(self, request):
+        posts = Post.models.order_by('-data_post_creation')
+        p = Paginator(posts, 2)
+        posts = p.get_page(request.GET.get('page', 1))
+        data = {
+            'posts': posts,
+        }
+        return render(request, 'newspaper/posts.html', data)
