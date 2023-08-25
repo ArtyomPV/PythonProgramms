5bf7ffc179f646a4e3a2c454dd5d0e7b8d6ac22c
Artyom <itcoding76@gmail.com>
2023-08-25 08:40:58 +0200

    finished D5.4

--- SkillFactory/projectNewsPaper/projectNewsPaper/settings.py
+++ SkillFactory/projectNewsPaper/projectNewsPaper/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-*j*)a7_+m##!%an1s8hczj!rk1k0f6$z3jk#y9cb68bbfsg7bo
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -78,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projectNewsPaper.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -88,7 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -108,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -119,7 +114,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -134,10 +128,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SITE_ID = 1
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "/static/newspaper",
-    BASE_DIR / "/static/sign",
+    BASE_DIR/"static",
+    BASE_DIR/'/static/newspaper',
+    BASE_DIR/"/static/sign",
 ]
 
-LOGIN_URL = 'sign/login/' # 4
+LOGIN_URL = '/sign/login'  # 4
 LOGIN_REDIRECT_URL = '/'
